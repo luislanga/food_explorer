87a948b2ff2d5bf4a783422212f008f2a32909f8
Luis Langa <luis.langajr96@gmai.com>
2023-03-04 20:29:51 -0300

    added signup validation

--- src/pages/Sign/SignUp/index.jsx
+++ src/pages/Sign/SignUp/index.jsx
@@ -2,18 +2,33 @@ import { Container, LogForm } from "./styles";
 import { Input } from "../../../components/Input";
 import { Button } from "../../../components/Button";
 import { Logo } from "../../../components/Logo";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { api } from "../../../services/api"
 
 export function SignUp(){
+    const navigate = useNavigate()
+
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     function handleSignUp(){
-        
+        if(!name || !email || !password) {
+            return alert("Preencha todos os campos!")
+        }
         api.post("/users", {name, email, password, is_admin: 0})
+            .then(() => {
+                alert("Usuário cadastrado com sucesso")
+                navigate("/")
+            })
+            .catch((error) => {
+                if(error.response){
+                    alert(error.response.data.message)
+                } else{
+                    alert("Não foi possível cadastrar o usuário")
+                }
+            })
     }
     return(
         <Container>
@@ -28,13 +43,13 @@ export function SignUp(){
                 </div>
                 <div>
                     <h2>Email</h2>
-                    <Input onChange={e => setEmail(e.target.value)} placeholder="Exemplo: exemplo@exemplo.com.br"/>
+                    <Input onChange={e => setEmail(e.target.value)} type="email" placeholder="Exemplo: exemplo@exemplo.com.br"/>
                 </div>
                 <div>
                     <h2>Senha</h2>
                     <Input onChange={e => setPassword(e.target.value)} type="password" placeholder="No mínimo 6 caracteres"/>
                 </div>
-                <Button type="button" onClick={handleSignUp} title="Entrar"/>
+                <Button type="button" onClick={handleSignUp} title="Cadastrar"/>
                 <Link to="/">Já tenho uma conta</Link>
             </LogForm>
         </Container>
